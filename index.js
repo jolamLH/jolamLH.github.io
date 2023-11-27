675a64874825d43d6792a9a667ea1e9ff444de73
jolamLH <152037045+jolamLH@users.noreply.github.com>
2023-11-27 17:31:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log('hello world');
+var opposite; 
+var genre = “document.getElementById(“genre”).value;
 
+function reallyBurst () { 
+  if (genre === “Pop”){
+    let opposite = "K-Pop";
+  }
+}
+console.log(opposite); 
