cdaa265857dee3ab0437c92d6ffb42b221a0cc8c
jolamLH <152037045+jolamLH@users.noreply.github.com>
2023-11-27 20:36:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 console.log('hello world');
-var opposite; 
-var genre = “document.getElementById(“genre”).value;
+var opposite = ""; 
+var boolean isPop = document.getElementById("pop").value;
+var boolean isHipHop = document.getElementById("hiphop").value;
+var boolean isIndie = document.getElementById("indie").value;
+var boolean isRock = document.getElementById("rock").value;
+var boolean isRB = document.getElementById("rb").value;
 
 function reallyBurst() { 
-  if (genre === “Pop”){
+  if (isPop == pop){
     let opposite = "K-Pop";
+  }
+  if (isHipHop == hiphop){
+    let opposite = "Metal"; 
   }
 }
 console.log(opposite); 
